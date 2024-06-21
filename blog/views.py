f06fbb8bb7a835e1e5156ba0fe6e42acd3b8dcde
kmvit <kmv-it@yandex.ru>
2024-06-21 16:35:51 +0300

    Добавил цену

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,8 @@ class BlogDetail(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super(BlogDetail, self).get_context_data(**kwargs)
         context['category_list'] = Category.objects.all()
-        context['blog_list'] = Blog.objects.all().exclude(id=self.object.id)[:3]
+        context['blog_list'] = Blog.objects.all().exclude(id=self.object.id)[
+                               :3]
         return context
 
 
@@ -35,8 +36,10 @@ class CategoryList(generic.DetailView):
         context = super(CategoryList, self).get_context_data(**kwargs)
         context['category_list'] = Category.objects.all()
         try:
-            context['category'] = Category.objects.get(slug=self.kwargs['slug'])
-            context['blog_list'] = Blog.objects.filter(category__slug=self.kwargs['slug'])
+            context['category'] = Category.objects.get(
+                slug=self.kwargs['slug'])
+            context['blog_list'] = Blog.objects.filter(
+                category__slug=self.kwargs['slug'])
         except Category.DoesNotExist:
             Http404
         return context
